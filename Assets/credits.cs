70b6df1f8614282ae13bc4961e8bdbf91b3688d0
donyannn <80718744+donyannn@users.noreply.github.com>
2025-09-19 00:03:00 -0400

    updated credits

--- Assets/credits.cs
+++ Assets/credits.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class credits : MonoBehaviour
 {
+    /* Assets:
+     * balls: https://opengameart.org/content/orbs by Bluce
+     * background image: https://t3.ftcdn.net/jpg/05/94/40/38/360_F_594403823_LwthuBDW5UZOSnxBV8wRVZFcc6KIQTGh.jpg
+     * paddles: https://t4.ftcdn.net/jpg/14/78/70/93/360_F_1478709380_GKkD0AvDrkutXNaJdU89GfUrzXMaij8W.jpg
+     * walls/floor: https://static.wikia.nocookie.net/aesthetics/images/7/7d/Brown_Texture.jpg/revision/latest/thumbnail/width/360/height/450?cb=20201102152124
+     * ball sound effect: Ping Pong ball being hit by paddle.wav by Monster_1999_Kyle -- https://freesound.org/s/653392/ -- License: Attribution NonCommercial 4.0
+     * background music: A Story of the Sea by code_box -- https://freesound.org/s/520193/ -- License: Creative Commons 0
+     * 
+     */
+
     // Start is called before the first frame update
     void Start()
     {
